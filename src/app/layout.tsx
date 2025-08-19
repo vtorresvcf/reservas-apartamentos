049f94a1cf57e98eacafd79e054236100e541e0e
Vicente Torres <vtorresvcf@gmail.com>
2025-08-19 10:54:46 +0200

    primer commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "700"] });
+
+export const metadata: Metadata = {
+  title: "Apartamentos Mushus",
+  description: "Alquiler de apartamentos turísticos en la playa",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <p>cuerpo con fuente Inter</p>
+      </main>
+    </div>
+  );
+}
